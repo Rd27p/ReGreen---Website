d65d86f74ebcfd8b5c7bc765e54dd9eb8451ec12
edselspth <edselspth@gmail.com>
2025-10-25 11:20:12 +0700

    feat: [ESH] HTML, CSS, and JS for login

---
+++ Login/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const loginForm = document.getElementById('loginForm');
+    const loginMessage = document.getElementById('loginMessage');
+
+    loginForm.addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        if (email === "" || password === "") {
+            loginMessage.innerHTML = `<div class="alert alert-danger" role="alert">Email dan Kata Sandi tidak boleh kosong.</div>`;
+        } else if (email === "admin@regreen.com" && password === "admin123") {
+            loginMessage.innerHTML = `<div class="alert alert-success" role="alert">Login berhasil! Mengalihkan...</div>`;
+            
+            setTimeout(() => {
+                window.location.href = '../Dashboard/dashboard.html'; 
+            }, 2000);
+
+        } else {
+            // Skenario Gagal
+            loginMessage.innerHTML = `<div class="alert alert-danger" role="alert">Email atau Kata Sandi salah.</div>`;
+        }
+    });
+});
