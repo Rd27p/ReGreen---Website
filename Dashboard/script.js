03c4952fd321a33788c70ba86f8395158e638f9a
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-10-25 13:01:06 +0700

    feat: [GAR] penambahan script.js untuk tombol logout di dashboard(25-10-2025)

---
+++ Dashboard/script.js
@@ -0,0 +1,6 @@
+document.getElementById("logoutBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (confirm("Apakah Anda yakin ingin logout?")) {
+    window.location.href = "../Login/login.html";
+  }
+});
