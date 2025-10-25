cec5aa05bc0838489c3490202cfcac5a082a3f3e
RezaIndraMaulana <rezaindramaulana9@gmail.com>
2025-10-25 16:50:26 +0700

    [RZZ] menambahkan halaman keuntungan
    Menambahkan halaman keuntungan

---
+++ Keuntungan/script.js
@@ -0,0 +1,9 @@
+document.getElementById("logoutBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    if (confirm("Apakah Anda yakin ingin logout?")) {
+      window.location.href = "../Login/login.html";
+    }
+  });
+
+  
+  
